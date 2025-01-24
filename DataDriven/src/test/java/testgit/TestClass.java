feb738f580d33ffcbecaf4c0e8510e99e09de121
amiryala <amarendar.miryala@davita.com>
2025-01-23 18:49:42 -0600

    committing code to feature branch

--- DataDriven/src/test/java/testgit/TestClass.java
+++ DataDriven/src/test/java/testgit/TestClass.java
@@ -4,7 +4,9 @@ import org.testng.reporters.jq.Main;
 
 public class TestClass {
 public static void main(String[] args) {
-	System.out.println("Hello GIT");
+	System.out.println("Hello GIT"); 
+	System.out.println("creating feature branch and committing code to feature branch"); 
+	
 }
 	
 }
