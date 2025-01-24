97b4ec2d227765a7d90a16131ddf4a14af850624
bhavanisoft5969 <bhavanisoft5969@gmail.com>
2025-01-23 19:11:02 -0600

    Update TestClass.java

--- DataDriven/src/test/java/testgit/TestClass.java
+++ DataDriven/src/test/java/testgit/TestClass.java
@@ -5,7 +5,7 @@ import org.testng.reporters.jq.Main;
 public class TestClass {
 public static void main(String[] args) {
 	System.out.println("Hello GIT"); 
-	System.out.println("creating feature branch and committing code to feature branch"); 
+	System.out.println("creating feature branch and committing code to feature branch !! Pull"); 
 	
 	                    
 	
